21653870df4583d8f3d3108439e49e33cce3ab3e
turarrauan <78708311+turarrauan@users.noreply.github.com>
2021-03-31 19:16:51 +0600

    Add files via upload

---
+++ list_1_02_05.py
@@ -0,0 +1,4 @@
+emojixpress = [2.26, 19.1, 25.6, 233.0, 15.2, 22.7, 64.6, 87.5, 6.81, 6.0]
+
+total = emojixpress[0] + emojixpress[1] + emojixpress[2] + emojixpress[3] + emojixpress[4] # < напишите код здесь >
+print('{:.2f}'.format(total))
